38a578e771a9454e9d410fcbd9819dff1873bb42
Ruslan <your_email@example.com>
2026-08-02 21:03:22 +0300

    homework_13

---
+++ homework_13/cart/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class CartAddBookForm(forms.Form):
+    quantity = forms.IntegerField(
+        min_value=1,
+        initial=1
+    )
+
+    override = forms.BooleanField(
+        required=False,
+        initial=False,
+        widget=forms.HiddenInput
+    )
