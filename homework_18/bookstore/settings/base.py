d86df85ef0b4df30e62b5f9655ef53889fb2d44d
Ruslan <your_email@example.com>
2026-09-17 08:12:58 +0300

    Fix Redis configuration for CI

--- homework_18/bookstore/settings/base.py
+++ homework_18/bookstore/settings/base.py
@@ -222,9 +222,15 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_BROKER_URL = os.getenv(
+    "CELERY_BROKER_URL",
+    "redis://redis:6379/0",
+)
 
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND",
+    "redis://redis:6379/0",
+)
 
 
 SENTRY_DSN = os.getenv("SENTRY_DSN")
